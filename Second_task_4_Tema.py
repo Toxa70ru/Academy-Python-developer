b0a12fe09ed40747875de64fca55cb07fc0cde20
Toxa70ru <42457726+Toxa70ru@users.noreply.github.com>
2022-11-02 17:08:10 +0700

    Delete Second_task_4_Tema.py

--- Second_task_4_Tema.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-number_X = randint(0,100)
-number_Y = randint(0,100)
-
-cel_rezalt = number_X // number_Y
-ostatok = number_X - (cel_rezalt * number_Y)
-print("Результат:")
-print(cel_rezalt,",",ostatok)
