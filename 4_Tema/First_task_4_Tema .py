f2dc8dd99a117bbf4dfee44bdff34bd8324bf16d
Toxa70ru <42457726+Toxa70ru@users.noreply.github.com>
2022-11-02 17:19:19 +0700

    Первое задание

---
+++ 4_Tema/First_task_4_Tema .py
@@ -0,0 +1,7 @@
+from random import randint
+first_per = randint(0,100)
+second_per = randint(0,100)
+third_per = randint(0,100)
+
+rezalt = (first_per + second_per + third_per) / 3
+print ("Среднее значение чисел =", rezalt)
